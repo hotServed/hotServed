0bc07ebe14cf45077675f2b5eca9d26ca5ff1519
harshoo3 <harshseta003@gmail.com>
2020-03-10 10:14:52 +0530

    help ho gaya achha khasa

---
+++ help/help.js
@@ -0,0 +1,171 @@
+
+function display1(){
+    var a=document.getElementById("FAQ-content");
+    a.style.display="none";
+    var b=document.getElementById("issue-content");
+    b.style.display="none";
+    var c=document.getElementById("Terms-content");
+    c.style.display="none";
+    var d=document.getElementById("rate-content");
+    d.style.display="none";
+    var e=document.getElementById("about-us-content");
+    e.style.display="none";
+    var f=document.getElementById("Account-settings-content");
+    f.style.display = "inline";
+    var g=document.getElementById("Account-settings");
+    g.style.color = "#c26733";
+    var h=document.getElementById("FAQ");
+    h.style.color = "#432616";
+    var i=document.getElementById("issue");
+    i.style.color = "#432616";
+    var j=document.getElementById("Terms");
+    j.style.color = "#432616";
+    var k=document.getElementById("rate");
+    k.style.color = "#432616";
+    var l=document.getElementById("about-us");
+    l.style.color = "#432616";
+}
+function display2(){
+    var a=document.getElementById("Account-settings-content");
+    a.style.display="none";
+    var b=document.getElementById("issue-content");
+    b.style.display="none";
+    var c=document.getElementById("Terms-content");
+    c.style.display="none";
+    var d=document.getElementById("rate-content");
+    d.style.display="none";
+    var e=document.getElementById("about-us-content");
+    e.style.display="none";
+    var f=document.getElementById("FAQ-content");
+    f.style.display = "inline";
+    var g=document.getElementById("FAQ");
+    g.style.color = "#c26733";
+    var h=document.getElementById("Account-settings");
+    h.style.color = "#432616";
+    var i=document.getElementById("issue");
+    i.style.color = "#432616";
+    var j=document.getElementById("Terms");
+    j.style.color = "#432616";
+    var k=document.getElementById("rate");
+    k.style.color = "#432616";
+    var l=document.getElementById("about-us");
+    l.style.color = "#432616";
+    
+}
+function display3(){
+    var a=document.getElementById("FAQ-content");
+    a.style.display="none";
+    var b=document.getElementById("Account-settings-content");
+    b.style.display="none";
+    var c=document.getElementById("Terms-content");
+    c.style.display="none";
+    var d=document.getElementById("rate-content");
+    d.style.display="none";
+    var e=document.getElementById("about-us-content");
+    e.style.display="none";
+    var f=document.getElementById("issue-content");
+    f.style.display = "inline";
+    var g=document.getElementById("issue");
+    g.style.color = "#c26733";
+    var h=document.getElementById("FAQ");
+    h.style.color = "#432616";
+    var i=document.getElementById("Account-settings");
+    i.style.color = "#432616";
+    var j=document.getElementById("Terms");
+    j.style.color = "#432616";
+    var k=document.getElementById("rate");
+    k.style.color = "#432616";
+    var l=document.getElementById("about-us");
+    l.style.color = "#432616";
+}
+function display4(){
+    var a=document.getElementById("FAQ-content");
+    a.style.display="none";
+    var b=document.getElementById("issue-content");
+    b.style.display="none";
+    var c=document.getElementById("Account-settings-content");
+    c.style.display="none";
+    var d=document.getElementById("rate-content");
+    d.style.display="none";
+    var e=document.getElementById("about-us-content");
+    e.style.display="none";
+    var f=document.getElementById("Terms-content");
+    f.style.display = "inline";
+    var g=document.getElementById("Terms");
+    g.style.color = "#c26733";
+    var h=document.getElementById("FAQ");
+    h.style.color = "#432616";
+    var i=document.getElementById("issue");
+    i.style.color = "#432616";
+    var j=document.getElementById("Account-settings");
+    j.style.color = "#432616";
+    var k=document.getElementById("rate");
+    k.style.color = "#432616";
+    var l=document.getElementById("about-us");
+    l.style.color = "#432616";
+}
+function display5(){
+    var a=document.getElementById("FAQ-content");
+    a.style.display="none";
+    var b=document.getElementById("issue-content");
+    b.style.display="none";
+    var c=document.getElementById("Terms-content");
+    c.style.display="none";
+    var d=document.getElementById("Account-settings-content");
+    d.style.display="none";
+    var e=document.getElementById("about-us-content");
+    e.style.display="none";
+    var f=document.getElementById("rate-content");
+    f.style.display = "inline";
+    var g=document.getElementById("rate");
+    g.style.color = "#c26733";
+    var h=document.getElementById("FAQ");
+    h.style.color = "#432616";
+    var i=document.getElementById("issue");
+    i.style.color = "#432616";
+    var j=document.getElementById("Terms");
+    j.style.color = "#432616";
+    var k=document.getElementById("Account-settings");
+    k.style.color = "#432616";
+    var l=document.getElementById("about-us");
+    l.style.color = "#432616";
+}
+function display6(){
+    var a=document.getElementById("FAQ-content");
+    a.style.display="none";
+    var b=document.getElementById("issue-content");
+    b.style.display="none";
+    var c=document.getElementById("Terms-content");
+    c.style.display="none";
+    var d=document.getElementById("rate-content");
+    d.style.display="none";
+    var e=document.getElementById("Account-settings-content");
+    e.style.display="none";
+    var f=document.getElementById("about-us-content");
+    f.style.display = "inline";
+    var g=document.getElementById("about-us");
+    g.style.color = "#c26733";
+    var h=document.getElementById("FAQ");
+    h.style.color = "#432616";
+    var i=document.getElementById("issue");
+    i.style.color = "#432616";
+    var j=document.getElementById("Terms");
+    j.style.color = "#432616";
+    var k=document.getElementById("rate");
+    k.style.color = "#432616";
+    var l=document.getElementById("Account-settings");
+    l.style.color = "#432616";
+}
+
+// function display1(){
+//     var f=document.getElementsByClassName("Account-settings-content");
+//     f.style.display = "block";
+// }
+// function display2(){
+//     var a=document.getElementsByClassName("FAQ-content");
+//     a.style.display = "block";
+// }
+// function display3(){
+//     var a=document.getElementsByClassName("Account-settings-content");
+//     a.style.display = "block";
+// }
